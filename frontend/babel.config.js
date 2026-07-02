0c43371d9d0aadf778fe2e8fd0be2fe4f7a0efe6
J Angga Wijaya <103455042+jejevj@users.noreply.github.com>
2026-07-02 23:46:28 +0700

    fix: correct babel.config.js for nativewind, add placeholder assets

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,10 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
+      [
+        'babel-preset-expo',
+        { jsxImportSource: 'nativewind' }
+      ]
     ],
     plugins: [
-      "nativewind/babel",
-    ],
+      'react-native-reanimated/plugin'
+    ]
   };
 };
